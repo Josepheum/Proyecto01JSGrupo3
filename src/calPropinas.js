7c71d74709de870abc1a7b84996966ee9905ffac
Nahuel Troncoso <92763369+NSTDEV@users.noreply.github.com>
2023-09-08 21:29:16 -0300

    Modificado los nombres, agregado el js y reorganizado el styles
    Styles en carpeta de estilos.
    Agregado el js con la lógica y añadida en el index.
    Modificado el estilo en general y acomodado el html.

--- src/calPropinas.js
+++ src/calPropinas.js
@@ -1,3 +1,11 @@
-/* Calculadora de propinas */
+function calcularPropina() {
+    var totalBalance = parseFloat(document.getElementById("total").value);
+    var tipPercentage = parseFloat(document.getElementById("percentage").value);
 
-/* Grupo 3 */
+    var tip = (totalBalance * tipPercentage) / 100;
+
+    var totalPay = totalBalance + tip;
+
+    document.getElementById("tip").textContent = tip.toFixed(2) + " USD";
+    document.getElementById("totalPay").textContent = totalPay.toFixed(2) + " USD";
+}
